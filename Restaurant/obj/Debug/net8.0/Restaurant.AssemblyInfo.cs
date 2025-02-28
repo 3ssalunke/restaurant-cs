a48e8999663a9e94da27de21f7b6ce15782656ac
3ssalunke <surajsalunke1276@gmail.com>
2025-02-28 08:40:31 +0530

    280225: create and run migrations and seeds

--- Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe72e99bbeec6044b90c94e22f9e460598d3301c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
